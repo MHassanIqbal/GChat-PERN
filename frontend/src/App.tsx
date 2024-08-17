d526b83b52870398be5d761bedb22659c0e59853
MHassanIqbal <mhassaniqbal22@gmail.com>
2024-08-17 21:39:03 +0100

    Frontend UI Design

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+
+const App = () => {
+  return (
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
